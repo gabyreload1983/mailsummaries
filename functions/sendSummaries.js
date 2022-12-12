bca8b26faf8515f6453fe1dbda1602bb1b3bbe99
Gabriel Sinapsis <gabyreload@gmail.com>
2022-12-12 14:16:10 -0300

    Service and function to send Summaries by mail

---
+++ functions/sendSummaries.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const { sendMail } = require("../services/sendMail");
+
+exports.sendSummaries = async (debtors) => {
+  try {
+    let countMails = 0;
+    for (const debtor of debtors) {
+      const body = `
+                <h3>ESTE ES UN MAIL AUTOMATICO.</h3>
+                <p>Nos comunicamos desde Sinapsis Srl</p>
+                <h4>Cliente: ${debtor.nombre}</h4>
+                <p>El <b>saldo</b> de la cuenta es de: <strong>$${debtor.saldo}</strong></p>
+                <p>Se <b>adjunta</b> resumen de cuenta.</p>
+                <p>Saludos coordiales</p>`;
+      const subject = `✔ RESUMEN DE CUENTA - ${debtor.nombre}`;
+      const date = new Date().toISOString().slice(0, 10);
+      const filePath = `${process.env.FILE_PATH_PREFIX}${date}-${debtor.codigo}.xlsx`;
+      await sendMail("", body, subject, filePath);
+      // await sendMail(customer.mail, body, subject, filePath);
+      countMails += 1;
+    }
+    console.log(
+      `${new Date().toLocaleString()} - Se enviaron ${countMails} mails con sus respectivos saldos`
+    );
+
+    const body = `<h5>Se enviaron ${countMails} mails con sus respectivos saldos.</h5>`;
+    sendMail(process.env.MAIL_INFO, body, "INFO SALDOS CC");
+  } catch (error) {
+    console.log("sendSummaries", error);
+    sendMail(process.env.MAIL_INFO, error.message, "ERROR!!!");
+  }
+};
---
+++ services/sendMail.js
@@ -0,0 +1,26 @@
+"use strict";
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASSWORD,
+  },
+});
+
+exports.sendMail = (mail, body, subject, filePath = "") => {
+  return transporter.sendMail({
+    from: process.env.MAIL_FROM,
+    to: mail,
+    cc: process.env.MAIL_CC,
+    subject: subject,
+    html: body,
+    attachments: {
+      path: filePath,
+    },
+  });
+};
