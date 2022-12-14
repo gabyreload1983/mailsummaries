27530f6d522cfc04107b43b170740b671151faba
Gabriel Sinapsis <gabyreload@gmail.com>
2022-12-14 08:04:26 -0300

    Adding process.exit

--- index.js
+++ index.js
@@ -4,16 +4,25 @@ const { exportSummaries } = require("./functions/exportSummaries");
 const { sendSummaries } = require("./functions/sendSummaries");
 
 async function app() {
-  console.log(`${new Date().toLocaleString()} - START`);
+  try {
+    console.log(`${new Date().toLocaleString()} - START`);
 
-  const debtors = await getDebtors();
-  console.log(`${new Date().toLocaleString()} - ${debtors.length} DEBTORS`);
+    const debtors = await getDebtors();
+    console.log(`${new Date().toLocaleString()} - ${debtors.length} DEBTORS`);
 
-  await exportSummaries(debtors);
-  console.log(`${new Date().toLocaleString()} - EXPORT EXCEL FILES COMPLETED`);
+    await exportSummaries(debtors);
+    console.log(
+      `${new Date().toLocaleString()} - EXPORT EXCEL FILES COMPLETED`
+    );
 
-  await sendSummaries(debtors);
-  console.log(`${new Date().toLocaleString()} - SENDING SUMMARIES COMPLETED`);
+    await sendSummaries(debtors);
+    console.log(`${new Date().toLocaleString()} - SENDING SUMMARIES COMPLETED`);
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    await sendMail(process.env.MAIL_INFO, error.message, "ERROR!!!");
+    process.exit(1);
+  }
 }
 
 app();
