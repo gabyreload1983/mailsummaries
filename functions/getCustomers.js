37c781875aef44da1085709651b0fae2fffc75e5
Gabriel Sinapsis <gabyreload@gmail.com>
2022-12-15 08:54:24 -0300

    Adding EXCLUDE_CUSTOMERS const

--- functions/getCustomers.js
+++ functions/getCustomers.js
@@ -1,12 +1,12 @@
 const { getData } = require("./getData");
-const excludedCustomers = process.env.EXCLUDE_CUSTOMERS;
+const EXCLUDE_CUSTOMERS = [];
 
 exports.getCustomers = async () => {
   try {
     const query = `SELECT * FROM clientes`;
     const customers = await getData(query);
     return customers.filter(
-      (customer) => !excludedCustomers.includes(customer.codigo)
+      (customer) => !EXCLUDE_CUSTOMERS.includes(customer.codigo)
     );
   } catch (error) {
     console.log("getCustomers", error);
